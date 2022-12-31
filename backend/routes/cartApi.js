0d5230194042d7f261f44e0880420b7da1c63e32
rohail321 <qrohail32@gmail.com>
2022-12-31 22:13:41 +0000

    update4

--- backend/routes/cartApi.js
+++ backend/routes/cartApi.js
@@ -6,7 +6,7 @@ const Cart = require('../models/Cart')
 const auth=require('../middleware/authorization')
 
 
-router.get('/',async (res,req)=>{
+router.get('/',auth,async (res,req)=>{
     try {
         const userId=req.user.userId
         const carts=await Cart.find({userId})
@@ -20,7 +20,7 @@ router.get('/',async (res,req)=>{
         let products=[]
         let result={}
         if(!isEmpty(retrievedCard)){
-            products=retrievedCard.products.map(product=>Product.findById({_id:product}) )
+            products=retrievedCard.products.map(async (product)=>await Product.findById({_id:product}) )
             products= await Promise.all(products)
             result={...retrievedCard.toJSON(),products}
         }
@@ -36,6 +36,41 @@ router.put('/:id',auth, async (req,res)=>{
         const product=req.body.product
         const cart=await Cart.update({_id:cartId},{$pullAll:{products:[product]}})
         res.send({cart})
+    } catch (err) {
+        res.status(500).json({msg:"server issue"})
+
+    }
+})
+
+router.post('/', auth ,async (req,res)=>{
+    try {
+        const userId=req.user.id
+        const {products}=req.body
+        let cart
+        let unfulfilledCart
+        const carts=await Cart.find({userId})
+        const haveValidCart=carts.reduce((acc,value)=>{
+            if(!value.fulfilled){
+                unfulfilledCart=value
+            }
+            return acc && value.fulfilled
+        },true)
+        if(haveValidCart){
+            cart=new Cart({userId,products})
+            cart=await cart.save()
+        }
+        else{
+            const stringProduct=[
+                ...unfulfilledCart.products,
+                ...products
+            ].map(product=>product.toString())
+            const newProduct=Array.from(new Set(stringProduct))
+            cart =await Cart.findByIdAndUpdate({_id:unfulfilledCart._id},{products:newProduct})
+
+        }
+        let value=cart.products.map(async (id)=>await Product.findById(id))
+        value=await Promise.all(value)
+        res.send({...cart.toJSON(),products:value})
     } catch (err) {
         res.status(500).json({msg:"server issue"})
 
